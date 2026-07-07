6aad2e643e50604c28b57c9d9fb6dce99e917b9f
Erick Kanja <e.kanja@alustudent.com>
2026-07-07 21:44:00 +0200

    Remove leading icon in apbar

--- lib/screens/startup/edit_startup_profile_screen.dart
+++ lib/screens/startup/edit_startup_profile_screen.dart
@@ -36,7 +36,7 @@ class _EditStartupProfileScreenState extends State<EditStartupProfileScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: const Icon(Icons.arrow_back, color: Colors.black),
+        // leading: const Icon(Icons.arrow_back, color: Colors.black),
         title: Text(
           'ALU Launchpad',
           style: Theme.of(context).textTheme.titleMedium?.copyWith(
