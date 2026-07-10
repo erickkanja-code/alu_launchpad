e5ecb3981a2d4c3c228ab8dfbc9955429282d9a8
Erick Kanja <e.kanja@alustudent.com>
2026-07-10 22:04:17 +0200

    Add firebase initialization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // add this line
+
 }
 
 android {
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/opportunity_provider.dart';
 import 'providers/application_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
